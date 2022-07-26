2f47d246c7b1416d571997fc74cf12060c8b06df
paruchuriharsha <paruchuriharsha97@gmail.com>
2022-07-26 20:07:13 +0530

    using local storage to save

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 (function(window, document, undefined){
   let store = {};
     window.onload = init;    
-      function init(){       
-        document.getElementById('sendbtn').addEventListener('click',sendValue);
+      function init(){     
+        store = getPostsFromStorage();  
+        document.getElementById('sendbtn').addEventListener('click',createPost);
         document.getElementById('posts').addEventListener('click',delNode);
+        let keys = Object.keys(store);
+        keys.forEach(key => {
+          createPostElement(store[key]);
+        });
       } 
 
-      function sendValue(e){
+      function createPost(e){
         const title = (document.getElementById('title').value);
         const text = (document.getElementById('text').value);
         if(text.replace(/\s+/g,'').length==0) return;
@@ -15,21 +20,19 @@
           title: title,
           text: text
         }
+        store[post.id] = post;
+        storePostsToStorage(store);
         document.getElementById('title').value = "";
         document.getElementById('text').value = "";
-
-        element = createPostElement(post);
-        const posts = document.getElementById('posts');
-        if(posts.firstChild){
-        posts.insertBefore(element,posts.firstChild);
-        }
-        else posts.appendChild(element);
+        createPostElement(post);
     }
 
     function delNode(e){
       let target = e.target;
       if(target.className == 'post-del'){
         id = (target.parentNode.parentNode.parentNode.id);
+        delete store[id];
+        storePostsToStorage(store);
         removeElement(document.getElementById(id));
       }
     }
@@ -55,6 +58,22 @@
         element.appendChild(textNode);
         element.classList.add('post');
         element.setAttribute('id', post.id);
-        return element;
-      }   
+        const posts = document.getElementById('posts');
+        if(posts.firstChild){
+          posts.insertBefore(element,posts.firstChild);
+        }
+        else posts.appendChild(element);
+      }
+
+      function getPostsFromStorage(){
+        const postObj = localStorage.getItem('ToDoNotes');
+        if(postObj!==undefined && postObj!==null){
+          return JSON.parse(postObj);
+        }
+        else return {};
+      }
+      
+      function storePostsToStorage(object){
+        localStorage.setItem('ToDoNotes',JSON.stringify(object));
+      }
 })(window, document, undefined);
